f50e9c5d43d3220bf1eb349ae4767e9396becdaf
Lium1126 <yosi.4sya@gmail.com>
2021-11-19 12:19:52 +0900

    バケットソートからシェルソートに変更

--- sort.cpp
+++ sort.cpp
@@ -10,34 +10,19 @@ namespace githubPractice
 	 */
 	std::vector<int> sort(std::vector<int> data)
 	{
-
-		// calculate bucket size
-		int bucket_size = data[0];
-		for (int i = 1; i < data.size(); i++)
+		for (int h = data.size() / 2; h > 0; h /= 2)
 		{
-			if (data[i] > bucket_size)
+			for (int i = h; i < data.size(); i += 1)
 			{
-				bucket_size = data[i];
-			}
-		}
-		bucket_size += 1;
+				int k = data[i];
 
-		// create empty buckets
-		std::vector<int> bucket[bucket_size];
+				int j;
+				for (j = i; j >= h && data[j - h] > k; j -= h)
+				{
+					data[j] = data[j - h];
+				}
 
-		// put data elements into buckets depending on the value
-		for (int i = 0; i < data.size(); i++)
-		{
-			bucket[data[i]].push_back(data[i]);
-		}
-
-		// concatenate all buckets into data
-		int id = 0;
-		for (int i = 0; i < bucket_size; i++)
-		{
-			for (int j = 0; j < bucket[i].size(); j++)
-			{
-				data[id++] = bucket[i][j];
+				data[j] = k;
 			}
 		}
 
