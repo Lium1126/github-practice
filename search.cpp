0f04d7300a166f87168f23cc522bb613c7a9094f
MiyamuP <pazu119@icloud.com>
2021-11-25 17:38:53 +0900

    change to sentinel

--- search.cpp
+++ search.cpp
@@ -5,19 +5,23 @@ namespace githubPractice
 {
 	/**
 	 * dataの中からtargetを探す
-	 * 
+	 *
 	 * @param[in] vector<int> data 探索元データ
 	 * @param[in] int target 探索対象データ
 	 * @return bool targetがdataに含まれていればtrueを返す
 	 */
 	bool search(std::vector<int> data, int target)
 	{
-		for (int x : data)
-		{
-			if (x == target)
-				return true;
-		}
+		int i = 0;
+		std::vector<int> cp(data.size());
+		std::copy(data.begin(), data.end(), cp.begin());
 
-		return false;
+		// set guard
+		cp.push_back(target);
+
+		while (cp[i] != target)
+			i++;
+
+		return i != data.size();
 	}
 }
