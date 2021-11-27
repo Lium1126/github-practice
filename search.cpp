c9fb80c4c8616a861928b39f22711d2f416f0c96
Lium1126 <yosi.4sya@gmail.com>
2021-11-27 12:14:25 +0900

    change linner search

--- search.cpp
+++ search.cpp
@@ -12,17 +12,10 @@ namespace githubPractice
 	 */
 	bool search(std::vector<int> data, int target)
 	{
-		int left = 0, right = data.size(), mid;
-
-		while (left < right)
+		for (int x : data)
 		{
-			mid = (left + right) / 2;
-			if (data[mid] == target)
+			if (x == target)
 				return true;
-			else if (target < data[mid])
-				right = mid;
-			else if (data[mid] < target)
-				left = mid + 1;
 		}
 
 		return false;
